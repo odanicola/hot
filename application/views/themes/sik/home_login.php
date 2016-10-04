905615cfd7843d163aa65ffd6d0a9192452f09f1
Oda Nicola <odanicola@gmail.com>
2016-10-04 08:30:26 +0700

    coba test

--- application/views/themes/sik/home_login.php
+++ application/views/themes/sik/home_login.php
@@ -5,8 +5,9 @@
     <title>{title}</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <link rel="icon" href="<?php echo base_url()?>public/themes/login/img/favicon.ico">
+    <!-- test -->
     <style type="text/css">
-      /* <![CDATA[ */    
+      /* <![CDATA[ */
         @import url(<?php echo base_url()?>public/themes/sik/bootstrap/css/bootstrap.min.css);
         @import url(<?php echo base_url()?>public/themes/login/css/style.css);
         @import url(<?php echo base_url()?>plugins/js/jqwidgets/styles/jqx.base.css);
