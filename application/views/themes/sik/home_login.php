5ced58dcd995ec27452b95ac3f31c29c393f87a3
Oda Nicola <odanicola@gmail.com>
2016-10-04 08:38:04 +0700

    tambah commit

--- application/views/themes/sik/home_login.php
+++ application/views/themes/sik/home_login.php
@@ -5,7 +5,7 @@
     <title>{title}</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <link rel="icon" href="<?php echo base_url()?>public/themes/login/img/favicon.ico">
-    <!-- test oda -->
+    <!-- test oda commit -->
     <style type="text/css">
       /* <![CDATA[ */
         @import url(<?php echo base_url()?>public/themes/sik/bootstrap/css/bootstrap.min.css);
